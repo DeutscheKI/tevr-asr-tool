076be220660234542cbb5f8107b9b7fbbc2907a2
Hajo Nils Krabbenhöft <hnk.git@hajo.me>
2022-08-09 11:42:53 +0200

    load multiple acoustic models + correctly load language model

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
-#include <iostream>
-#include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
+
+#include "absl/flags/flag.h"
+#include "absl/flags/internal/commandlineflag.h"
+#include "absl/flags/internal/private_handle_accessor.h"
+#include "absl/flags/reflection.h"
+#include "absl/flags/usage_config.h"
+#include "absl/memory/memory.h"
+#include "absl/strings/match.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/string_view.h"
 #include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/delegates/hexagon/hexagon_delegate.h"
 #include "tensorflow/lite/interpreter.h"
 #include "tensorflow/lite/interpreter_builder.h"
 #include "tensorflow/lite/kernels/kernel_util.h"
 #include "tensorflow/lite/kernels/register.h"
-#include "tensorflow/lite/kernels/test_util.h"
 #include "tensorflow/lite/model_builder.h"
 #include "tensorflow/lite/testing/util.h"
 #include "tensorflow/lite/tools/benchmark/benchmark_utils.h"
@@ -16,33 +23,68 @@
 #include "tensorflow/lite/kernels/register.h"
 #include "tensorflow/lite/model.h"
 #include "tensorflow/lite/optional_debug_tools.h"
+#include "kenlm/lm/ngram_query.hh"
 
 #define FATAL_ERRORS(errormsg) {fprintf(stderr, "FATAL ERROR (line %d): %s\n", __LINE__, (errormsg)); exit(1);}
 #define FATAL_ERROR(errormsg) FATAL_ERRORS((errormsg).c_str())
 
 ABSL_FLAG(std::string, data_folder_path, "/usr/share/tevr_asr_tool", "Path to the data folder.");
 
+const char* tokens[]= {"", "", " ", "chen", "sche", "lich", "isch", "icht", "iche", "eine", "rden", "tion", "urde", "haft", "eich", "rung",
+                       "chte", "ssen", "chaf", "nder", "tlic", "tung", "eite", "iert", "sich", "ngen", "erde", "scha", "nden", "unge", "lung",
+                       "mmen", "eren", "ende", "inde", "erun", "sten", "iese", "igen", "erte", "iner", "tsch", "keit", "der", "die", "ter",
+                       "und", "ein", "ist", "den", "ten", "ber", "ver", "sch", "ung", "ste", "ent", "ach", "nte", "auf", "ben", "eit", "des",
+                       "ers", "aus", "das", "von", "ren", "gen", "nen", "lle", "hre", "mit", "iel", "uch", "lte", "ann", "lie", "men", "dem",
+                       "and", "ind", "als", "sta", "elt", "ges", "tte", "ern", "wir", "ell", "war", "ere", "rch", "abe", "len", "ige", "ied",
+                       "ger", "nnt", "wei", "ele", "och", "sse", "end", "all", "ahr", "bei", "sie", "ede", "ion", "ieg", "ege", "auc", "che",
+                       "rie", "eis", "vor", "her", "ang", "f\u00fcr", "ass", "uss", "tel", "er", "in", "ge", "en", "st", "ie", "an", "te",
+                       "be", "re", "zu", "ar", "es", "ra", "al", "or", "ch", "et", "ei", "un", "le", "rt", "se", "is", "ha", "we", "at", "me",
+                       "ne", "ur", "he", "au", "ro", "ti", "li", "ri", "eh", "im", "ma", "tr", "ig", "el", "um", "la", "am", "de", "so", "ol",
+                       "tz", "il", "on", "it", "sc", "sp", "ko", "na", "pr", "ni", "si", "fe", "wi", "ns", "ke", "ut", "da", "gr", "eu", "mi",
+                       "hr", "ze", "hi", "ta", "ss", "ng", "sa", "us", "ba", "ck", "em", "kt", "ka", "ve", "fr", "bi", "wa", "ah", "gt", "di",
+                       "ab", "fo", "to", "rk", "as", "ag", "gi", "hn", "s", "t", "n", "m", "r", "l", "f", "e", "a", "b", "d", "h", "k", "g",
+                       "o", "i", "u", "w", "p", "z", "\u00e4", "\u00fc", "v", "\u00f6", "j", "c", "y", "x", "q", "\u00e1", "\u00ed",
+                       "\u014d", "\u00f3", "\u0161", "\u00e9", "\u010d", "?" };
+
 int main(int argc, char** argv) {
     ::tflite::LogToStderr();
     absl::ParseCommandLine(argc, argv);
-
     const std::string &data_folder_path = absl::GetFlag(FLAGS_data_folder_path);
-    const std::string &model_path = data_folder_path + std::string("/acoustic_model.tflite");
-    std::unique_ptr<tflite::FlatBufferModel> model = tflite::FlatBufferModel::BuildFromFile(model_path.c_str());
-    if(model == nullptr) FATAL_ERROR(std::string("Could not load ")+model_path);
 
-    tflite::ops::builtin::BuiltinOpResolver resolver;
-    tflite::InterpreterBuilder builder(*model, resolver);
-    std::unique_ptr<tflite::Interpreter> interpreter;
-    builder(&interpreter);
-    if(interpreter != nullptr) FATAL_ERRORS("Could not create interpreter for acoustic model.");
+    std::vector<std::unique_ptr<tflite::FlatBufferModel>> acoustic_models;
+    for(int i=0;i<4;i++) {
+        const std::string &model_path = data_folder_path + std::string("/acoustic_model_0")+std::to_string(i)+std::string(".tflite");
+        acoustic_models.emplace_back(tflite::FlatBufferModel::BuildFromFile(model_path.c_str()));
+        if(acoustic_models[i] == nullptr) FATAL_ERROR(std::string("Could not load ")+model_path);
+    }
+
+    std::vector<std::unique_ptr<tflite::Interpreter>> acoustic_interpreters;
+    for(int i=0;i<acoustic_models.size();i++) {
+        acoustic_interpreters.emplace_back();
+        tflite::ops::builtin::BuiltinOpResolver resolver;
+        tflite::InterpreterBuilder builder(*acoustic_models[i], resolver);
+        builder(&acoustic_interpreters[i]);
+        if(acoustic_interpreters[i] == nullptr) FATAL_ERROR(std::string("Could not create interpreter for acoustic model ")+std::to_string(i));
+    }
+
+    lm::ngram::Config config;
+    const std::string &lm_path = data_folder_path + std::string("/language_model.bin");
+    lm::ngram::TrieModel language_model(lm_path.c_str(), config);
+
+    int token_idx = language_model.GetVocabulary().Index(StringPiece("mückenstiche"));
+    if(token_idx != 68501) FATAL_ERRORS("Language model vocabulary is wrong.");
+
+/*
+    int wav_size = 12123;
+
+    if(interpreter->ResizeInputTensor(0, {1, wav_size}) != kTfLiteOk) FATAL_ERRORS("Could not resize audio input tensor.");
     if(interpreter->AllocateTensors() != kTfLiteOk) FATAL_ERRORS("Could not allocate tensors for acoustic model.");
 
-    // T* input = interpreter->typed_input_tensor<T>(i);
+    float* audio_input = interpreter->typed_input_tensor<float>(0);
 
     if(interpreter->Invoke() != kTfLiteOk) FATAL_ERRORS("Could not invoke acoustic model.");
 
-    // T* output = interpreter->typed_output_tensor<T>(i);
-
+    float* logits = interpreter->typed_output_tensor<float>(0);
+*/
 
 }
